0613a495a439a5eccd2dd47d1e689891127a6752
Carlos Dantas <carlos.d.dantas6@aluno.senai.br>
2026-02-05 17:06:41 -0300

    Finalização so site com bulma

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, redirect, render_template, request
+
+
+app = Flask(__name__)
+
+app.secret_key = "banana"
+
+lista_de_comentario = []
+
+@app.route("/")
+def pagina_principal():
+    return render_template("principal.html")
+
+@app.route("/sobremim", methods= ["GET"])
+def pagina_sobremim():
+    return render_template("sobremim.html")
+
+@app.route("/login", methods= ["GET"])
+def pagina_login():
+    return render_template("login.html")
+
+@app.route("/login", methods= ["POST"])
+def login_post():
+    usuario = request.form.get("usuario")
+    senha = request.form.get("senha")
+
+    if usuario == "Carlos" and senha == "123":
+
+        return redirect("/comentarios")
+    else:
+        return redirect("/login")
+    
+@app.route("/comentarios", methods= ["GET"])
+def pagina_comentarios():
+    return render_template("comentarios.html", lista_de_comentario = lista_de_comentario)
+
+@app.route("/adicionarcomentarios", methods= ["POST"])
+def adicionar_comentarios():
+    comentario = request.form.get("cmt")
+    lista_de_comentario.append(comentario)
+    print(lista_de_comentario)
+    return redirect("/comentarios")
+
+
+
+
+
+app.run(debug=True)
